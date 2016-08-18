babc8ad5617877cc193a8ea41e62d4d175275926
Ty <tywalker@outlook.com>
2016-08-18 08:19:01 -0700

    push to git

--- HelloWOrld/ViewController.swift
+++ HelloWOrld/ViewController.swift
@@ -18,8 +18,14 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
-
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 }
 
